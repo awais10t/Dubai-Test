f0bcf5c32b1492462683e01d4ac5bf7ebb5cca0d
awais10t <awais10t@gmail.com>
2024-11-15 12:57:22 +0500

    Added Canvas,cards,timer object and make a Layout plan

---
+++ Assets/Scripts/gameManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gameManager : MonoBehaviour
+{
+    public Sprite[] cardFace;
+    public Sprite cardBack;
+    public GameObject[] cards;
+    public GameObject gameTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
