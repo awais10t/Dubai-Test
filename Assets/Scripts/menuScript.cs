2bf081142e5e4825e15dbf10eb80896d9f975b33
awais10t <awais10t@gmail.com>
2024-11-16 03:49:40 +0500

    Menu Scene Added

---
+++ Assets/Scripts/menuScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class menuScript : MonoBehaviour {
+
+	public void triggerMenu(int trigger) {
+		switch (trigger) {
+		case(0) :
+			SceneManager.LoadScene("GameScene");
+			break;
+		case(1) :
+			Application.Quit();
+			break;
+		}
+	}
+}
